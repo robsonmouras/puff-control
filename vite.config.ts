b69192a3622b90daff88ba93addfadec6f3469d1
Robson Moura <robson.moura88@gmail.com>
2025-03-12 15:20:40 -0300

    Configuração para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   base: 'https://github.com/robsonmouras/puff-control.git', // Substitua pelo nome do seu repositório
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Garante que @ aponte para src/
+    },
+  },
 })
